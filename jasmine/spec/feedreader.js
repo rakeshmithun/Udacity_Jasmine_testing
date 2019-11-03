0551c744f2c0943f1566174ccb32c888f40cb67a
Rakesh Mithun <slipknut@gmail.com>
2019-11-04 07:55:57 +1000

    04112019_755am
    - Updated menu tests as per review comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -52,15 +52,17 @@ $(function() {
 
     /* Write a new test suite named "The menu" */
     describe('The menu', function() {
-        const menu = document.querySelector('body');
+
+        let body = $('body'),
+            menu_button = $('.menu-icon-link');
 
         /* Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-        it('is hidden by default', function() {
-            expect(menu.classList).toContain("menu-hidden");
+        it('should hide the menu by default', function() {
+            expect(body.hasClass('menu-hidden')).toBeTruthy();
         });
 
         /* Write a test that ensures the menu changes
@@ -68,15 +70,14 @@ $(function() {
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
          */
-        it('shows when the menu icon is clicked', function() {
-            menu.classList.toggle('menu-hidden');
-            expect(menu.classList.contains('menu-hidden')).toBe(false);
+        it('should show menu when click the menu icon link and hide the menu when click again', function() {
+            menu_button.click();
+            expect(body.hasClass('menu-hidden')).toBeFalsy();
+
+            menu_button.click();
+            expect(body.hasClass('menu-hidden')).toBeTruthy();
         });
 
-        it('hides when the menu icon is clicked again', function() {
-            menu.classList.toggle('menu-hidden');
-            expect(menu.classList.contains('menu-hidden')).toBe(true);
-        });
     });
     /* Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
